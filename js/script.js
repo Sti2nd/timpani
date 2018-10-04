86cabe1f64d4d3001a85b0867cfa04c5c34335fa
Stian Jørgensrud <stianahj@gmail.com>
2018-10-04 20:22:59 +0200

    Add full scale from C2 to G3

--- js/script.js
+++ js/script.js
@@ -1,54 +1,145 @@
-// Sounds from https://freesound.org/search/?a_tag=&a_username=&g=1&f=grouping_pack%3A%221548_timpani%22+&a_filename=&a_soundid=&q=timpani&s=%22avg_rating+desc%22&a_packname=&a_description=&advanced=0&page=2#sound
-
-var timp_b = new Howl({
-    src: ["audio/timp_b_strong.flac"],
-    html5: true,
-    preload: true
-})
-var timp_f = new Howl({
-    src: ["audio/timp_f_strong.flac"],
+let timp_c2 = new Howl({
+    src: ["audio/timpani/C2-PB.wav"],
     html5: true,
     preload: true
 })
 
-var timp_g = new Howl({
-    src: ["audio/timp_g_strong.flac"],
+let timp_db2 = new Howl({
+    src: ["audio/timpani/Db2-PB.wav"],
     html5: true,
     preload: true
 })
 
-var timp_dsharp = new Howl({
-    src: ["audio/timp_dsharp_strong.flac"],
+let timp_d2 = new Howl({
+    src: ["audio/timpani/D2-PB.wav"],
     html5: true,
     preload: true
 })
 
-var timp_csharp = new Howl({
-    src: ["audio/timp_csharp_strong.flac"],
+let timp_eb2 = new Howl({
+    src: ["audio/timpani/Eb2-PB.wav"],
     html5: true,
     preload: true
 })
 
-var timp_c3 = new Howl({
-    src: ["audio/timp_C3_strong.wav"],
+let timp_e2 = new Howl({
+    src: ["audio/timpani/E2-PB.wav"],
     html5: true,
     preload: true
 })
 
-var timp_c4 = new Howl({
-    src: ["audio/timp_C4_strong.wav"],
+let timp_f2 = new Howl({
+    src: ["audio/timpani/F2-PB.wav"],
     html5: true,
     preload: true
 })
+
+let timp_gb2 = new Howl({
+    src: ["audio/timpani/Gb2-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_g2 = new Howl({
+    src: ["audio/timpani/G2-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_ab2 = new Howl({
+    src: ["audio/timpani/Ab2-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_a2 = new Howl({
+    src: ["audio/timpani/A2-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_bb2 = new Howl({
+    src: ["audio/timpani/Bb2-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_b2 = new Howl({
+    src: ["audio/timpani/B2-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_c3 = new Howl({
+    src: ["audio/timpani/C3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_db3 = new Howl({
+    src: ["audio/timpani/Db3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_d3 = new Howl({
+    src: ["audio/timpani/D3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_eb3 = new Howl({
+    src: ["audio/timpani/Eb3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_e3 = new Howl({
+    src: ["audio/timpani/E3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_f3 = new Howl({
+    src: ["audio/timpani/F3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_gb3 = new Howl({
+    src: ["audio/timpani/Gb3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
+let timp_g3 = new Howl({
+    src: ["audio/timpani/G3-PB.wav"],
+    html5: true,
+    preload: true
+})
+
 
 let timpaniSounds = {
-    "Timpani in B" : timp_b,
-    "Timpani in F" : timp_f,
-    "Timpani in G" : timp_g,
-    "Timpani in D#" : timp_dsharp,
-    "Timpani in C#" : timp_csharp,
-    "Timpani in C3" : timp_c3,
-    "Timpani in C4" : timp_c4
+    "C2 - Timpani" : timp_c2,
+    "C#2 - Timpani" : timp_db2,
+    "D2 - Timpani" : timp_d2,
+    "Eb2 - Timpani" : timp_eb2,
+    "E2 - Timpani" : timp_e2,
+    "F2 - Timpani" : timp_f2,
+    "F#2 - Timpani" : timp_gb2,
+    "G2 - Timpani" : timp_g2,
+    "G#2 - Timpani" : timp_ab2,
+    "A2 - Timpani" : timp_a2,
+    "Bb2 - Timpani" : timp_bb2,
+    "B2 - Timpani" : timp_b2,
+    "C3 - Timpani" : timp_c3,
+    "C#3 - Timpani" : timp_db3,
+    "D3 - Timpani" : timp_d3,
+    "Eb3 - Timpani" : timp_eb3,
+    "E3 - Timpani" : timp_e3,
+    "F3 - Timpani" : timp_f3,
+    "F3# - Timpani" : timp_gb3,
+    "G3 - Timpani" : timp_g3
 }
 
 let timpaniSelect1 = document.getElementById("timpaniSelectpauk1")
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Timpani online
+I created this web page because my student orchestra thought it would be fun to have timpani.
+
+The sounds are from No Budget Orchestra. The sample pack did not contain recording of every note, and thus sometimes one sample have been used for two notes. Here are an overview of the original sounds, and what sample have been used for another sound (like it probably is being used in sfz synthezisers).
+
+_timpani.sfz_ from No Budget Orchestra\
+\<control> default_path=Timpani\\\
+\<group> ampeg_sustain=0 ampeg_hold=0 ampeg_decay=20 ampeg_release=3\
+\<region> sample=C2.wav lokey=a1 hikey=c2 pitch_keycenter=c2\
+\<region> sample=D2.wav lokey=c#2 hikey=d2 pitch_keycenter=d2\
+\<region> sample=E2.wav lokey=d#2 hikey=e2 pitch_keycenter=e2\
+\<region> sample=F2.wav lokey=f2 hikey=f#2 pitch_keycenter=f2\
+\<region> sample=Ab2.wav lokey=g2 hikey=g#2 pitch_keycenter=g#2\
+\<region> sample=A2.wav lokey=a2 hikey=a2 pitch_keycenter=a2\
+\<region> sample=Bb2.wav lokey=a#2 hikey=a#2 pitch_keycenter=a#2\
+\<region> sample=C3.wav lokey=b2 hikey=c3 pitch_keycenter=c3\
+\<region> sample=D3.wav lokey=c#3 hikey=d#3 pitch_keycenter=d3\
+\<region> sample=Gb3.wav lokey=e3 hikey=f#3 pitch_keycenter=f#3\
+\<region> sample=G3.wav lokey=g3 hikey=g3 pitch_keycenter=g3\
+\<region> sample=Ab3.wav lokey=g#3 hikey=g#3 pitch_keycenter=g#3\
+\<region> sample=A3.wav lokey=a3 hikey=a3 pitch_keycenter=a3\
+\<region> sample=Bb3.wav lokey=a#3 hikey=d4 pitch_keycenter=a#3
